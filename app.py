0d267f9ee779e8193bc6e16a478c6a5576012c82
shellkore <shellkoresahu@gmail.com>
2020-01-03 00:43:16 +0530

    added flask cors

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 from quoter import quoteLister
 import random
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def success():
 	return render_template("home.html")
 
 @app.route('/quote', methods =['GET'])
+
 def get_quote():
 	tag = request.args['tag']
 	currentQuoteList = quoteLister(tag)
